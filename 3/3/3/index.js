f145c2652c934faa2526f247dc164dfd93f82c4b
Kameil <130675534+Kameil@users.noreply.github.com>
2024-09-10 14:36:55 -0300

    Update index.js

--- 3/3/3/index.js
+++ 3/3/3/index.js
@@ -1,1 +1,45 @@
+function salvetodos(track) {
+    switch (track) {
+        case 1:
+            window.open('https://open.spotify.com/intl-pt/track/4ssHL7bSnOAE7HjiuLx6Co')
+            break;
+        case 2:
+            window.open("https://open.spotify.com/intl-pt/track/2fjqfFUq64thFsqfKN01kS");
+            break;
+        case 3:
+            window.open("https://open.spotify.com/intl-pt/track/3Sa2gVQioE31DcG4nKNDqD");
+            break;
+        case 4:
+            window.open("https://open.spotify.com/intl-pt/track/6QsHhB0W9GtLF3cI7mUVkQ");
+            break;
+        case 5:
+            window.open("https://open.spotify.com/intl-pt/track/6OcZP4rUfuxms2r8Mwx5BQ");
+            break;
+        case 6:
+            window.open("https://open.spotify.com/intl-pt/track/4v0VOuuaZSQHjnjzkC3cnk")
+            break;
+        case 7:
+            window.open("https://open.spotify.com/intl-pt/track/2fTTmjyF7pfarDBAbsJkvb")
+            break;
+        case 8:
+            window.open("https://open.spotify.com/intl-pt/track/3Kh5a6vXMiJtC35jMEfmyy")
+            break;
+        case 9:
+            window.open("https://open.spotify.com/intl-pt/track/7pMfMzfYSTbNxj5lJEbQIp")
+            break;
+        case 10:
+            window.open("https://open.spotify.com/intl-pt/track/2ux3Sb90r5aA74nLMls1u0")
+            break;
+        case 11:
+            window.open('https://open.spotify.com/intl-pt/track/0qsKefQyXCzaxjHbOcd8IU')
+            break;
+        case 12:
+            window.open("https://open.spotify.com/intl-pt/track/49gGZLLm1d7fBFQkTqskXo");
+            break;
+        default:
+            window.open('https://open.spotify.com/intl-pt/album/5CP84pfi4AFe95UTACFWrk?uid=e79434869ccfbb6b4450&uri=spotify%3Atrack%3A4ssHL7bSnOAE7HjiuLx6Co')
+            break;
 
+    }
+    window.open("https://open.spotify.com/intl-pt/album/5CP84pfi4AFe95UTACFWrk?uid=e79434869ccfbb6b4450&uri=spotify%3Atrack%3A4ssHL7bSnOAE7HjiuLx6Co");
+}
